6f13e22405583116e795492c4813790b39dc2df2
MariaMelkonyan <maria_melkonyan@edu.aua.am>
2016-11-27 18:30:58 +0400

    Update Math.java

--- utils/Math.java
+++ utils/Math.java
@@ -1,1 +1,19 @@
+package Utils;
 
+public class Math {
+	
+	 public static int factorialR (int n) {
+		if (n == 0) {
+			return 1;
+		}
+		return n * factorialR(n-1); 
+	}
+	
+	public static int factorialLoop (int n) {
+		int num = 1;
+		for (int i = 1; i <= n; i++) {
+			num *= i;
+		}	
+		return num;
+	}
+}
