e76649789c4c084021c7aed0750c00b9eb439b29
MariaMelkonyan <maria_melkonyan@edu.aua.am>
2016-11-27 18:29:45 +0400

    Update Square.java

--- Shapes/Square.java
+++ Shapes/Square.java
@@ -1,1 +1,23 @@
+package Shapes;
 
+public class Square {
+
+    private int length;
+
+    public Square() {
+    }
+
+    public int getArea() {
+        return this.getLength() * this.getLength();
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        if(length >= 0) {
+            this.length = length;
+        }
+    }
+}
