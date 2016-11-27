dfc93c3a30562be0b9db6b86eaecff47b7c3fd2d
MariaMelkonyan <maria_melkonyan@edu.aua.am>
2016-11-27 18:29:18 +0400

    Update Rectangle.java

--- Shapes/Rectangle.java
+++ Shapes/Rectangle.java
@@ -1,1 +1,28 @@
+package Shapes;
 
+public class Rectangle {
+
+    private int length;
+    private int height;
+
+    public Rectangle() {
+    }
+
+    public int getArea() {
+        return this.length * this.height;
+    }
+
+    public int getLength() {
+        return length;
+    }
+    public void setLenghtHeight(int length, int height) {
+        if(length >= 0 && height >= 0) {
+            this.length = length;
+            this.height = height;
+        }
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
