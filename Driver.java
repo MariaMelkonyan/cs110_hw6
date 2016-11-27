741aad7e4745781ed573ab180c66f31f6e7b84da
MariaMelkonyan <maria_melkonyan@edu.aua.am>
2016-11-27 18:30:26 +0400

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -1,1 +1,23 @@
+import Shapes.Square;
+import Shapes.Rectangle;
 
+public class Driver {
+
+    public static void main (String[] args) {
+        Square Sq = new Square();
+        Sq.setLength(100);
+        int SqArea = Sq.getArea();
+
+        Rectangle Rec = new Rectangle();
+        Rec.setLenghtHeight(50, 80);
+        int RecArea = Rec.getArea();
+
+        System.out.println(SqArea);
+        System.out.println(RecArea);
+        
+        double N = 15.6;
+	    System.out.println(Math.factorialR((int)N));
+		System.out.println(Math.factorialLoop((int)N));
+        }
+    }
+}
